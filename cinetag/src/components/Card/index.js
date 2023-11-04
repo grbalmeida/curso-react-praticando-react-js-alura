1639904d6d8d5699af525f750d04807cdc336c77
grbalmeida <g.r.almeida@live.com>
2023-11-04 11:55:01 -0300

    Favoritar

--- cinetag/src/components/Card/index.js
+++ cinetag/src/components/Card/index.js
@@ -1,7 +1,14 @@
+import { useFavoritoContext } from 'contextos/Favoritos';
 import styles from './Card.module.css';
 import iconeFavoritar from './favoritar.png';
+import iconeDesfavoritar from './desfavoritar.png';
 
 function Card({ id, titulo, capa }) {
+
+    const { favorito, adicionarFavorito } = useFavoritoContext();
+    const ehFavorito = favorito.some(fav => fav.id === id);
+    const icone = ehFavorito ? iconeDesfavoritar : iconeFavoritar;
+
     return (
         <div className={styles.container}>
             <img
@@ -11,9 +18,10 @@ function Card({ id, titulo, capa }) {
             />
             <h2>{titulo}</h2>
             <img
-                src={iconeFavoritar}
+                src={icone}
                 alt="Favoritar filme"
                 className={styles.favoritar}
+                onClick={() => adicionarFavorito({ id, titulo, capa })}
             />
         </div>
     )
