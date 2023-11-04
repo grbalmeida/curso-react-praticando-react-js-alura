7c3d9e93187ef54cc9870fe1e67f1f28aa665d26
grbalmeida <g.r.almeida@live.com>
2023-11-04 08:34:18 -0300

    Página de favoritos

--- cinetag/src/pages/Favoritos/index.js
+++ cinetag/src/pages/Favoritos/index.js
@@ -1,9 +1,19 @@
 import styles from './Favoritos.module.css';
 
+import Banner from 'components/Banner';
+import Titulo from 'components/Titulo';
+import Card from 'components/Card';
+
 function Favoritos() {
     return (
         <>
-        
+            <Banner imagem="favoritos" />
+            <Titulo>
+                <h1>Meus Favoritos</h1>
+            </Titulo>
+            <section className={styles.container}>
+                <Card id="1" titulo="Um componente no espaço" capa="https://caelum-online-public.s3.amazonaws.com/2802-react-praticando/img1.png" />
+            </section>
         </>
     )
 }
