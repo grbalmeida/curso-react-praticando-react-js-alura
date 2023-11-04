2672d274ebaf16939802ebcab67699f663fff345
grbalmeida <g.r.almeida@live.com>
2023-11-04 11:57:49 -0300

    Lista de favoritos

--- cinetag/src/pages/Favoritos/index.js
+++ cinetag/src/pages/Favoritos/index.js
@@ -3,8 +3,12 @@ import styles from './Favoritos.module.css';
 import Banner from 'components/Banner';
 import Titulo from 'components/Titulo';
 import Card from 'components/Card';
+import { useFavoritoContext } from 'contextos/Favoritos';
 
 function Favoritos() {
+
+    const { favorito } = useFavoritoContext();
+
     return (
         <>
             <Banner imagem="favoritos" />
@@ -12,7 +16,9 @@ function Favoritos() {
                 <h1>Meus Favoritos</h1>
             </Titulo>
             <section className={styles.container}>
-                <Card id="1" titulo="Um componente no espaço" capa="https://caelum-online-public.s3.amazonaws.com/2802-react-praticando/img1.png" />
+                {favorito.map(fav => (
+                    <Card {...fav} key={fav.id} />
+                ))}
             </section>
         </>
     )
