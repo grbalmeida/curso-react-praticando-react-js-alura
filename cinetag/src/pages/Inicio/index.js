91e70df5640cbb8ae56dd1d20aaf174ff36142bf
grbalmeida <g.r.almeida@live.com>
2023-11-06 09:14:26 -0300

    useEffect

--- cinetag/src/pages/Inicio/index.js
+++ cinetag/src/pages/Inicio/index.js
@@ -1,12 +1,21 @@
+import { useEffect, useState } from 'react';
+
 import Banner from 'components/Banner';
 import Card from 'components/Card';
 import Titulo from 'components/Titulo';
 
 import styles from './Inicio.module.css';
 
-import videos from 'json/db.json';
-
 function Inicio() {
+
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        fetch('https://my-json-server.typicode.com/grbalmeida/cinetag-api/videos')
+            .then(resposta => resposta.json())
+            .then(dados => setVideos(dados));
+    }, []);
+
     return (
         <>
             <Banner imagem="home" />
